95edfa0a0441c6a3c8341712514fe6676da4c36a
Fabio Maienschein <fma@objektkultur.de>
2019-08-15 15:05:50 +0200

    Further styles

--- src/components/BookItem.jsx
+++ src/components/BookItem.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import useGlobal from "../store";
+import { Link } from "react-router-dom";
 
 const BookItem = props => {
   const [globalState, globalActions] = useGlobal();
@@ -11,12 +12,14 @@ const BookItem = props => {
   };
 
   return (
-    <li className="bookItem" onClick={() => goToDetailPage()}>
-      <h4>
-        {book.author} - {book.title}
-      </h4>
-      <p>{book.description.slice(0, 120) + "..."}</p>
-    </li>
+    <Link to={"/details/" + book.id}>
+      <li className="bookItem" onClick={() => goToDetailPage()}>
+        <h4>
+          {book.author} - {book.title}
+        </h4>
+        <p>{book.description.slice(0, 120) + "..."}</p>
+      </li>
+    </Link>
   );
 };
 
